b8c8f2c7eafc7a295829e28d757489dc45fd00e5
Felle17 <felix.larsson17@gmail.com>
2023-08-23 12:52:41 +0200

    Mattematik test klart

--- www/public/index.php
+++ www/public/index.php
@@ -3,7 +3,7 @@
 <head lang="sv"></head>
 <body>
 <h1>Matematik-test</h1>
-<form action="evaluate.php" method="post">
+<form action="resultat.php" method="post">
    <fieldset>
        <legend>Frågor</legend>
        <label>4 + 5 = </label>
@@ -12,6 +12,15 @@
        <label>3 x 5 = </label>
        <input type="text" name="q2">
        <br>
+       <label> 8*3 = </label>
+       <input type="text" name="q3">
+       <br>
+       <label> 164 / 4 = </label>
+       <input type="text" name="q4">
+       <br>
+        <label>128 * 5 =</label>
+        <input type="text" name="q4">
+        <br>
        <input type="submit" value="Rätta">
    </fieldset>
 </form>
